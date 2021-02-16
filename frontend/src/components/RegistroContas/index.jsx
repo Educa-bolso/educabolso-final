0adf3a78c584d03b7c6c54f01fd082b4c0634b0c
Erica Andrade <erica.andradesa@gmail.com>
2021-02-16 18:55:45 -0300

    subindo aplicação

---
+++ frontend/src/components/RegistroContas/index.jsx
@@ -0,0 +1,18 @@
+import iconLixeira from '../../assets/lixeira.svg';
+import './style.css';
+
+const RegistroContas = () =>{
+    return(
+        <article>
+        <div class="card" style={{background: "#9159C1"}} >
+            <img style={{width:"15px"}} src={iconLixeira}/>
+            <h2>Luz</h2>
+            <p>R$ 80,00 / mês</p>
+            <button>Editar</button>
+        </div>
+        </article>
+    );
+}
+
+export default RegistroContas;
+
--- frontend/src/pages/Contas/index.jsx
+++ frontend/src/pages/Contas/index.jsx
@@ -1,12 +1,62 @@
+import {Form, Button} from 'react-bootstrap';
+
+
 import Header from '../../components/Header';
 import Navigation from '../../components/Navigation';
+import Footer from './../../components/Footer';
+import RegistroContas from './../../components/RegistroContas';
+
+
+import './styles.css';
 
 const Contas = () => {
   return (
     <>
       <Header />
       <Navigation />
-      <h1>Contas</h1>
+
+    <section className="">
+      <div className="contas">
+        <h3 className="contas1">Contas registradas: 11</h3>
+        <h3>Gasto mensal: R$ 200,00</h3>
+      </div>
+
+      <div className="registro-contas">
+        
+          <RegistroContas/>
+          <RegistroContas/>
+          <RegistroContas/>
+          <RegistroContas/>
+          <RegistroContas/>
+          <RegistroContas/>
+          <RegistroContas/>
+          <RegistroContas/>
+      
+      </div>
+
+        <span class="nenhum-cadastro">
+          <p>Nenhuma conta cadastrada!</p>
+        </span>
+
+
+
+    <main> 
+        <Form className="formulario">
+            <p>X</p>
+            <Form.Group controlId="formBasicText">
+              <Form.Label>Conta:</Form.Label>
+              <Form.Control className="input-form" type="text" />
+            </Form.Group>
+            <Form.Group controlId="formBasicText">
+              <Form.Label>Valor:</Form.Label>
+              <Form.Control className="input-form"   type="text" />
+            </Form.Group>
+            <button id="button" type="submit">Cadastrar</button>
+        </Form>
+    </main> 
+    </section>
+
+    <Footer/>
     </>
   );
 };
