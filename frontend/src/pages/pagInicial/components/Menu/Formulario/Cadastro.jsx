7fca48e0a6eb457e89c32e3137ef24b62b5825a4
Caio <caio.rodrigues.barbosa@usp.br>
2021-02-19 10:47:05 -0300

    adicionando axios para fazer requisiçoes para o backend

--- frontend/src/pages/pagInicial/components/Menu/Formulario/Cadastro.jsx
+++ frontend/src/pages/pagInicial/components/Menu/Formulario/Cadastro.jsx
@@ -1,47 +1,65 @@
 import React from 'react';
-import {Formik, Field, Form} from 'formik';
+import { Formik, Field, Form } from 'formik';
 import schemaCadastro from './schemaCadastro';
 
-
 function Cadastro() {
-    function onSubmit(values, actions){
-        console.log('SUBMIT', values);
+  function onSubmit(values, actions) {
+    console.log('SUBMIT', values);
+  }
 
-    }
-
-    return(
-        <div>
-            <Formik validationSchema={schemaCadastro} onSubmit={onSubmit} initialValues={{
-                nome: '',
-                email: '',
-                senha: '',
-                confirmarsenha: '',
-            }} render={({values, errors})=>(
-                <Form id="form2" action="" method="POST">
-                    <h2>Cadastro</h2>
-                    <div class="form-group">
-                        <Field class="form-control mb-4 mt-2" type="text" name="nome" placeholder="Digite seu Nome" required=""/>
-                        {errors.nome &&(
-                        <span>{errors.nome}</span>
-                    )}
-                        <Field class="form-control mb-4 mt-2" type="email" name="email" placeholder="Digite seu Email" required=""/>
-                        {errors.email &&(
-                        <span>{errors.email}</span>
-                    )}
-                        <Field class="form-control mb-4 mt-2" type="password" name="senha" placeholder="Digite digite sua Senha" required=""/>
-                        {errors.senha &&(
-                        <span>{errors.senha}</span>
-                    )}
-                        <Field class="form-control" type="password" name="confirmarsenha" placeholder=" Confirme sua Senha " required=""/>
-                        {errors.confirmarsenha &&(
-                        <span>{errors.confirmarsenha}</span>
-                    )}
-                    </div>
-                    <input id="button" class="botao" type="submit" value="Cadastrar"/>
-                </Form>
-
-            )}/>
-        </div>
-    );
+  return (
+    <div>
+      <Formik
+        validationSchema={schemaCadastro}
+        onSubmit={onSubmit}
+        initialValues={{
+          nome: '',
+          email: '',
+          senha: '',
+          confirmarsenha: '',
+        }}
+        render={({ values, errors }) => (
+          <Form id='form2' action='' method='POST'>
+            <h2>Cadastro</h2>
+            <div class='form-group'>
+              <Field
+                class='form-control mb-4 mt-2'
+                type='text'
+                name='nome'
+                placeholder='Digite seu Nome'
+                required=''
+              />
+              {errors.nome && <span>{errors.nome}</span>}
+              <Field
+                class='form-control mb-4 mt-2'
+                type='email'
+                name='email'
+                placeholder='Digite seu Email'
+                required=''
+              />
+              {errors.email && <span>{errors.email}</span>}
+              <Field
+                class='form-control mb-4 mt-2'
+                type='password'
+                name='senha'
+                placeholder='Digite digite sua Senha'
+                required=''
+              />
+              {errors.senha && <span>{errors.senha}</span>}
+              <Field
+                class='form-control'
+                type='password'
+                name='confirmarsenha'
+                placeholder=' Confirme sua Senha '
+                required=''
+              />
+              {errors.confirmarsenha && <span>{errors.confirmarsenha}</span>}
+            </div>
+            <input id='button' class='botao' type='submit' value='Cadastrar' />
+          </Form>
+        )}
+      />
+    </div>
+  );
 }
 export default Cadastro;
