937e35680032e45260c408bccbb30b11f6005752
BeatrizMiotto <miottobiasforever@gmail.com>
2021-02-19 17:47:57 -0300

    Validações tela perfil e feedback

--- frontend/src/pages/Feedback/index.jsx
+++ frontend/src/pages/Feedback/index.jsx
@@ -1,9 +1,14 @@
 import Header from '../../components/Header';
 import Navigation from '../../components/Navigation';
 import Footer from '../../components/Footer';
+import { Formik, Form } from 'formik';
+import schemaFeed from './schemaFeed';
 import './styles.css';
 
 const Feedback = () => {
+  function onSubmit(values, actions){
+
+  }
   return (
     <>
       <Header />
@@ -11,11 +16,20 @@ const Feedback = () => {
 
       <div>
           <section id="formu">
-          <form action="" method="POST">
-            <label for="feedback" id="texto">Deixe aqui sugestões ou reclamações<br /> para podermos melhorar nossa plataforma!</label><br />
-            <textarea className="formulario" id="feedback"></textarea><br />
-            <button id="botao" type="submit">Enviar</button>
-          </form>
+            <Formik validationSchema={schemaFeed} onSubmit={onSubmit} initialValues={{
+              msg: '',
+
+            }} render={({values, errors})=>(
+              <Form >
+                <label for="feedback" id="texto">Deixe aqui sugestões ou reclamações<br /> para podermos melhorar nossa plataforma!</label><br />
+                <textarea className="formulario" id="feedback" name="msg"></textarea><br />
+                 {errors.msg &&(
+                      <span>{errors.msg}</span>
+                  )}
+                <button id="botao" type="submit">Enviar</button>
+              </Form>
+            )}/>
+          
         </section>
 
       </div>
