a72b161aa1fc3de7da49900a87f34cbc65d0bfcb
Denis Moreira <denisnarcizo@gmail.com>
2021-02-21 09:16:27 -0300

    Mobile

--- mobile/src/Paginas/home.js
+++ mobile/src/Paginas/home.js
@@ -7,46 +7,27 @@ export default function App(){
   return (
     <SafeAreaView>
       <View style = {styles.header}>
-      <Text style = {styles.saudacao1} Text style ={{color:'#AC8F8F', fontSize:15}}>Boa tarde</Text>
-      <Text style = {styles.saudacao2} Text style ={{color:'#AC8F8F', fontSize:18, fontWeight:'bold'}}>Beatriz Japa</Text>
+      <Text style = {styles.saudacao1} Text style ={{color:'#AC8F8F', fontSize:15}}>Ola!!!</Text>
+      <Text style = {styles.saudacao2} Text style ={{color:'#AC8F8F', fontSize:18, fontWeight:'bold'}}>Bem vindo ao EducaBolso</Text>
         <Image
-        source={require('./assets/avatar1.png')}
+        source={require('./assets/logo.png')}
         style={{width: 50, height:50, marginRight:15}}
         resizeMode="contain"
         />
         </View>
       
-        <TouchableOpacity style={styles.btnMenu}>
-         <Text style={styles.registerText}>Contas</Text>
-         <Text style={styles.registerText}>Saiba Mais</Text>
-         </TouchableOpacity>
-
-         <View style = {styles.saudacao3}>
-        <Text style ={{color:'#AC8F8F', fontSize:25, fontWeight:'bold'}}>Deixe aqui sugestões ou reclamações para podermos melhorar nossa plataforma</Text>
-         </View>
-
-        <TextInput style={{
-          height:200,
-          margin:20,
-          padding:10,
-          borderColor:'#40B63E',
-          borderWidth:2,
-          marginTop:50,
-          textAlign: 'auto',
-          
-        }}
-        multiline={true}
-        editable={true}
-        borderBottomWidth={3}
-
+      <SafeAreaView style = {styles.EducaBolso}>
+      <Image 
+        source={require('./assets/Gif_Menor.gif')}
+        style={{width: 250, height:250, marginRight:15}}
+        resizeMode="contain"
         />
+      </SafeAreaView>
+        
+             
     
     
     
-    <TouchableOpacity style={styles.btnSubmit}>
-      <Text style={styles.submitText} Text style ={{color:'#FFF', fontSize:22, fontWeight:'bold'}}>Enviar</Text>
-    </TouchableOpacity>
-
     <View style = {styles.footer1}/>
 
     <SafeAreaView>
@@ -93,42 +74,7 @@ const styles = StyleSheet.create({
    
   },
 
-  input:{
-    backgroundColor:'#40B63E',
-    marginTop: 100,
-    width: '90%',
-    height:50,
-    marginBottom:115,
-    color: '#222',
-    fontSize: 17,
-    borderRadius:7,
-    padding:10,
-  },
-
-  btnSubmit:{
-    backgroundColor:'#40B63E',
-    width: 150,
-    height: 50,
-    alignItems:'center',
-    justifyContent:'center',
-    borderRadius:7,
-    marginHorizontal:'32%',
-    marginTop:40,
-   
-
-  },
-
-  btnMenu:{
-    backgroundColor:'#FFF',
-    marginTop:40,
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-between',
-    paddingRight:230,
-    paddingLeft:20,
-    marginTop: 10,
-  },
-
+  
   saudacao3: {
     alignItems:'center',
     justifyContent:'space-between',
@@ -154,6 +100,12 @@ const styles = StyleSheet.create({
     paddingRight:20,
     paddingLeft:20,
     marginTop:5,
+  },
+
+  EducaBolso:{
+    marginLeft:80,
+    marginTop:200,
+    marginBottom:150,
   }
 });
           
---
+++ mobile/src/Paginas/referencias.js
@@ -0,0 +1,418 @@
+import React from "react";
+import {
+  View,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  Button,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+
+export default function App() {
+  return (
+    <ScrollView>
+      <SafeAreaView>
+        <View style={styles.header}>
+          <Text
+            style={styles.saudacao1}
+            Text
+            style={{ color: "#AC8F8F", fontSize: 15 }}
+          >
+            Boa tarde
+          </Text>
+          <Text
+            style={styles.saudacao2}
+            Text
+            style={{ color: "#AC8F8F", fontSize: 18, fontWeight: "bold" }}
+          >
+            Beatriz Japa
+          </Text>
+          <Image
+            source={require("./assets/avatar1.png")}
+            style={{ width: 50, height: 50, marginRight: 15 }}
+            resizeMode="contain"
+          />
+        </View>
+
+        <TouchableOpacity style={styles.btnMenu}>
+          <Text style={styles.registerText}>Contas</Text>
+          <Text style={styles.registerText}>Saiba Mais</Text>
+        </TouchableOpacity>
+
+        <SafeAreaView style={styles.referencias}>
+          <View style={styles.perfil1}>
+            <Text
+              style={{ color: "#AC8F8F", fontSize: 25, fontWeight: "bold", marginLeft:110}}
+            >
+              Cadernos
+            </Text>
+          </View>
+          <SafeAreaView style={styles.cadernos1}>
+            <Text
+              style={{
+                color: "#FFF",
+                fontSize: 20,
+                fontWeight: "bold",
+                width: 110,
+              }}
+            >
+              Viver bem com o dinheiro que você tem
+            </Text>
+            <Image
+              source={require("./assets/caderno.png")}
+              style={{ width: 60, height: 60, marginRight: 15 }}
+              resizeMode="contain"
+            />
+          </SafeAreaView>
+          <SafeAreaView style={styles.blocosalvar}>
+            <TouchableOpacity style={styles.btnSalvar}>
+              <Text
+                style={styles.submitText}
+                Text
+                style={{ color: "#FFF", fontSize: 15, fontWeight: "bold" }}
+              >
+                Acesse aqui
+              </Text>
+            </TouchableOpacity>
+          </SafeAreaView>
+
+          <SafeAreaView style={styles.cadernos1}>
+            <Text
+              style={{
+                color: "#FFF",
+                fontSize: 20,
+                fontWeight: "bold",
+                width: 110,
+              }}
+            >
+              Gestão das finanças pessoais
+            </Text>
+            <Image
+              source={require("./assets/caderno.png")}
+              style={{ width: 60, height: 60, marginRight: 15 }}
+              resizeMode="contain"
+            />
+          </SafeAreaView>
+          <SafeAreaView style={styles.blocosalvar}>
+            <TouchableOpacity style={styles.btnSalvar}>
+              <Text
+                style={styles.submitText}
+                Text
+                style={{ color: "#FFF", fontSize: 15, fontWeight: "bold" }}
+              >
+                Acesse aqui
+              </Text>
+            </TouchableOpacity>
+          </SafeAreaView>
+
+          <View style={styles.perfil1}>
+            <Text
+              style={{ color: "#AC8F8F", fontSize: 25, fontWeight: "bold", marginLeft:130}}
+            >
+              Sites
+            </Text>
+          </View>
+          <SafeAreaView style={styles.sites1}>
+            <Text
+              style={{
+                color: "#FFF",
+                fontSize: 20,
+                fontWeight: "bold",
+                width: 110,
+              }}
+            >
+              Serasa Educação Financeira
+            </Text>
+            <Image
+              source={require("./assets/sites.png")}
+              style={{ width: 60, height: 60, marginRight: 15 }}
+              resizeMode="contain"
+            />
+          </SafeAreaView>
+          <SafeAreaView style={styles.blocosalvar2}>
+            <TouchableOpacity style={styles.btnSalvar2}>
+              <Text
+                style={styles.submitText}
+                Text
+                style={{ color: "#FFF", fontSize: 15, fontWeight: "bold" }}
+              >
+                Acesse aqui
+              </Text>
+            </TouchableOpacity>
+          </SafeAreaView>
+
+          <SafeAreaView style={styles.sites1}>
+            <Text
+              style={{
+                color: "#FFF",
+                fontSize: 20,
+                fontWeight: "bold",
+                width: 110,
+              }}
+            >
+              Como organizar o orçamento familiar - FGV
+            </Text>
+            <Image
+              source={require("./assets/sites.png")}
+              style={{ width: 60, height: 60, marginRight: 15 }}
+              resizeMode="contain"
+            />
+          </SafeAreaView>
+          <SafeAreaView style={styles.blocosalvar2}>
+            <TouchableOpacity style={styles.btnSalvar2}>
+              <Text
+                style={styles.submitText}
+                Text
+                style={{ color: "#FFF", fontSize: 15, fontWeight: "bold" }}
+              >
+                Acesse aqui
+              </Text>
+            </TouchableOpacity>
+          </SafeAreaView>
+          <View style={styles.perfil1}>
+            <Text
+              style={{ color: "#AC8F8F", fontSize: 25, fontWeight: "bold" , marginLeft:70 }}
+            >
+              Canais do Youtube
+            </Text>
+          </View>
+          <SafeAreaView style={styles.youtube1}>
+            <Text
+              style={{
+                color: "#FFF",
+                fontSize: 20,
+                fontWeight: "bold",
+                width: 110,
+              }}
+            >
+              NoFront - Empoderamento Financeiro
+            </Text>
+            <Image
+              source={require("./assets/youtube.png")}
+              style={{ width: 60, height: 60, marginRight: 15 }}
+              resizeMode="contain"
+            />
+          </SafeAreaView>
+          <SafeAreaView style={styles.blocosalvar3}>
+            <TouchableOpacity style={styles.btnSalvar}>
+              <Text
+                style={styles.submitText}
+                Text
+                style={{ color: "#FFF", fontSize: 15, fontWeight: "bold" }}
+              >
+                Acesse aqui
+              </Text>
+            </TouchableOpacity>
+          </SafeAreaView>
+
+          <SafeAreaView style={styles.youtube1}>
+            <Text
+              style={{
+                color: "#FFF",
+                fontSize: 20,
+                fontWeight: "bold",
+                width: 110,
+              }}
+            >
+              GUETONOMIA
+            </Text>
+            <Image
+              source={require("./assets/youtube.png")}
+              style={{ width: 60, height: 60, marginRight: 15 }}
+              resizeMode="contain"
+            />
+          </SafeAreaView>
+          <SafeAreaView style={styles.blocosalvar3}>
+            <TouchableOpacity style={styles.btnSalvar}>
+              <Text
+                style={styles.submitText}
+                Text
+                style={{ color: "#FFF", fontSize: 15, fontWeight: "bold" }}
+              >
+                Acesse aqui
+              </Text>
+            </TouchableOpacity>
+          </SafeAreaView>
+
+        </SafeAreaView>
+
+        <View style={styles.footer1} />
+
+        <SafeAreaView>
+          <View style={styles.footer}>
+            <Image
+              source={require("./assets/Home.png")}
+              style={{ width: 30, height: 30, marginRight: 15 }}
+              resizeMode="contain"
+            />
+
+            <Image
+              source={require("./assets/game.png")}
+              style={{ width: 30, height: 30, marginRight: 15 }}
+              resizeMode="contain"
+            />
+
+            <Image
+              source={require("./assets/Vector.png")}
+              style={{ width: 30, height: 30, marginRight: 15 }}
+              resizeMode="contain"
+            />
+          </View>
+        </SafeAreaView>
+      </SafeAreaView>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#FFF",
+    marginTop: 40,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingRight: 10,
+    paddingLeft: 10,
+    borderBottomWidth: 3,
+    borderColor: "#9AE06F",
+  },
+
+  input: {
+    width: "90%",
+    height: 50,
+    marginBottom: 15,
+    padding: 10,
+    borderRadius: 10,
+    borderBottomColor: "#40B63E",
+    borderBottomWidth: 5,
+  },
+
+  btnSalvar: {
+    backgroundColor: "#40B63E",
+    width: 120,
+    height: 30,
+    borderRadius: 7,
+    paddingLeft: 28,
+    paddingTop: 5,
+    marginLeft: 80,
+  },
+
+  btnMenu: {
+    backgroundColor: "#FFF",
+    marginTop: 40,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingRight: 230,
+    paddingLeft: 20,
+    marginTop: 10,
+  },
+
+  perfil1: {
+    marginLeft: 30,
+    paddingRight: 10,
+    marginTop: 50,
+    paddingLeft: 10,
+  },
+
+  footer1: {
+    backgroundColor: "#778899",
+    width: "90%",
+    height: 3,
+    marginLeft: 18,
+    marginTop: 60,
+  },
+
+  footer: {
+    backgroundColor: "#FFF",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingRight: 20,
+    paddingLeft: 20,
+    marginTop: 5,
+  },
+
+  cadernos1: {
+    backgroundColor: "#7159C1",
+    width: 290,
+    height: 100,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    marginLeft: 50,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingRight: 20,
+    paddingLeft: 20,
+    marginTop: 20,
+    paddingTop: 20,
+  },
+
+  blocosalvar: {
+    backgroundColor: "#7159C1",
+    width: 290,
+    height: 50,
+    marginLeft: 50,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  sites1:{
+    backgroundColor: "#50BC22",
+    width: 290,
+    height: 100,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    marginLeft: 50,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingRight: 20,
+    paddingLeft: 20,
+    marginTop: 20,
+    paddingTop: 20,
+  },
+  
+  blocosalvar2:{
+    backgroundColor: "#50BC22",
+    width: 290,
+    height: 50,
+    marginLeft: 50,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+
+  btnSalvar2:{
+    backgroundColor: "#7159C1",
+    width: 120,
+    height: 30,
+    borderRadius: 7,
+    paddingLeft: 28,
+    paddingTop: 5,
+    marginLeft: 80,
+  },
+  youtube1:{
+    backgroundColor: "#9E8FD2",
+    width: 290,
+    height: 100,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    marginLeft: 50,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingRight: 20,
+    paddingLeft: 20,
+    marginTop: 20,
+    paddingTop: 20,
+  },
+  
+  blocosalvar3:{
+    backgroundColor: "#9E8FD2",
+    width: 290,
+    height: 50,
+    marginLeft: 50,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  }
+});
